6b042623fa825e5e4cd4062f6d5f202f98b18500
Peter Hadorn <peter@hadornag.ch>
2015-01-03 17:43:45 +0100

    fixed apple touch icons and demo footer

--- wpYAML-child-demo/footer.php
+++ wpYAML-child-demo/footer.php
@@ -24,9 +24,9 @@
 				<div class="ym-gbox">				
 					<div class="footer">
 						<?php if ( ICL_LANGUAGE_CODE=='en' ) : ?>
-							<p class="copyright">wpYAML is a free theme brought to you by <a href="http://www.bizwp.com/themes/" title="WordPress Business Themes">WordPress Business Themes</a>, <a href="http://www.websitedesign.ch" title="Webdesign">Web Design</a> and <a href="http://www.hadornag.ch" title="Web Agency Switzerland Hadorn AG">Web Agency Switzerland</a>, feel free to use it and fork it.</p>
+							<p class="copyright">wpYAML is a free theme brought to you by <a href="http://www.bizwp.com/themes/" title="WordPress Business Themes">WordPress Business Themes</a>, <a href="http://www.websitedesign.ch" title="Webdesign">Web Design</a> and <a href="http://www.hadornag.ch" title="Web Agency Switzerland Hadorn AG">Hadorn AG</a>, feel free to use it and fork it.</p>
 						<?php else: ?>
-							<p class="copyright">wpYAML ist ein Gratis-Theme von <a href="http://www.wordpresstheme.ch" title="WordPress Themes Schweiz">WordPress Themes</a>, <a href="http://www.websitedesign.ch" title="Webdesign">Webdesign</a> und <a href="http://www.hadornag.ch" title="Webagentur Schweiz Hadorn AG">Webagentur Hadorn AG</a>, Du kannst es benutzen und forken.</p>
+							<p class="copyright">wpYAML ist ein Gratis-Theme von <a href="http://www.wordpresstheme.ch" title="WordPress Themes Schweiz">WordPress Themes</a>, <a href="http://www.websitedesign.ch" title="Webdesign">Webdesign</a> und <a href="http://www.hadornag.ch" title="Hadorn AG Webagentur">Hadorn AG</a>, Du kannst es benutzen und forken.</p>
 						<?php endif; ?>
 						<?php wp_footer(); ?>
 					</div>
--- wpYAML-child-demo/header.php
+++ wpYAML-child-demo/header.php
@@ -23,11 +23,18 @@
 		<meta name="MobileOptimized" content="320">
 		<meta name="viewport" content="width=device-width, initial-scale=1.0"/>
 
-		<link rel="apple-touch-icon" href="<?php echo get_template_directory_uri(); ?>/lib/images/apple-icon-touch.png">
-		<link rel="icon" href="<?php echo get_template_directory_uri(); ?>/favicon.png">
 		<!--[if IE]>
 			<link rel="shortcut icon" href="<?php echo get_template_directory_uri(); ?>/favicon.ico">
 		<![endif]-->
+		<link rel="apple-touch-icon" href="<?php echo get_template_directory_uri(); ?>/lib/images/apple-touch-icon.png">
+		<link rel="apple-touch-icon" sizes="57x57" href="<?php echo get_template_directory_uri(); ?>/lib/images/apple-touch-icon-57x57.png" />
+		<link rel="apple-touch-icon" sizes="72x72" href="<?php echo get_template_directory_uri(); ?>/lib/images/apple-touch-icon-72x72.png" />
+		<link rel="apple-touch-icon" sizes="76x76" href="<?php echo get_template_directory_uri(); ?>/lib/images/apple-touch-icon-76x76.png" />
+		<link rel="apple-touch-icon" sizes="114x114" href="<?php echo get_template_directory_uri(); ?>/lib/images/apple-touch-icon-114x114.png" />
+		<link rel="apple-touch-icon" sizes="120x120" href="<?php echo get_template_directory_uri(); ?>/lib/images/apple-touch-icon-120x120.png" />
+		<link rel="apple-touch-icon" sizes="144x144" href="<?php echo get_template_directory_uri(); ?>/lib/images/apple-touch-icon-144x144.png" />
+		<link rel="apple-touch-icon" sizes="152x152" href="<?php echo get_template_directory_uri(); ?>/lib/images/apple-touch-icon-152x152.png" />		
+
 		<link rel="alternate" type="application/rss+xml" title="<?php bloginfo('name'); ?> Feed" href="<?php echo home_url(); ?>/feed/">
 		<link rel="pingback" href="<?php bloginfo('pingback_url'); ?>">
 		<?php wp_head(); ?>
